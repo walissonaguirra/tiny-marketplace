fa46dd18715aef132c7c4fe1fa0aee32524a1dce
Walisson Aguirra <walisson.aguirra@gmail.com>
2022-05-11 10:29:06 -0300

    Atualização da página inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,16 @@
     @auth
         <a href="{{ route('dashboard') }}">Início</a>
     @else
+    <p>
         <a role="button" href="{{ route('login') }}">Entrar</a>
         <a role="button" class="outline" href="{{ route('register') }}">Cadastrar</a>
+    </p>
     @endauth
+
+    <p>
+        Este é um sistema de marketplace simples, a ideia é que qualquer pessoa possa
+        ter sua página de venda na internet com zero configuração, você pode anuncia
+        desde livros usado á qualquer tipo de produto e recebe sua solicitação de
+        compra no whatsapp.
+    </p>
 </x-app-layout>
